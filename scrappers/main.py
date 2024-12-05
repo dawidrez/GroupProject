3ef8a479b919b13e02cc496234a0c7c975dc8c2a
dawid.rezmer <dawid.rezmer1@gmail.com>
2024-12-05 15:51:52 +0100

    Scrap filmweb

--- scrappers/main.py
+++ scrappers/main.py
@@ -2,8 +2,10 @@ from film_saver import FilmSaver
 from scrappers import FilmwebScraper, IMDBScraper
 
 if __name__ == '__main__':
-    scrapper = IMDBScraper()
+    #scrapper = IMDBScraper()
+    scrapper = FilmwebScraper()
     scrapper.scrape()
     films = scrapper.get_films()
-    saver = FilmSaver(films, "imdb")
+    #saver = FilmSaver(films, "imdb")
+    saver = FilmSaver(films, "filmweb")
     saver.save_as_csv()
