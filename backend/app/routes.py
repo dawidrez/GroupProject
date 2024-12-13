740a151afcc04b5cfc5aff34445649f58183beb9
dawid.rezmer <dawid.rezmer1@gmail.com>
2024-12-13 12:55:10 +0100

    Fix Get endpoint

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 
+from .schemas import FilmSchema
 from .services.film_service import get_all_films
 
 film = Blueprint("film", __name__)
@@ -8,4 +9,5 @@ film = Blueprint("film", __name__)
 @film.route("/films", methods=["GET"])
 def list_films():
     films = get_all_films()
-    return jsonify([film.to_dict() for film in films])
+    film_schemas = [FilmSchema.from_orm(film).dict() for film in films]
+    return jsonify(film_schemas)
---
+++ backend/app/schemas.py
@@ -0,0 +1,27 @@
+import uuid
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class GenreSchema(BaseModel):
+    id: uuid.UUID
+    name: str
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class FilmSchema(BaseModel):
+    id: uuid.UUID
+    original_title: str
+    english_title: Optional[str]
+    year: int
+    filmweb_rating: Optional[float]
+    imdb_rating: Optional[float]
+    genres: List[GenreSchema]
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
