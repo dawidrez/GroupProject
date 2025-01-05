8b36d4dd2afbd21951ecfd2c2d65aa48e0723d17
dawid.rezmer <dawid.rezmer1@gmail.com>
2025-01-05 16:49:17 +0100

    Add filtering by genres

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -12,7 +12,8 @@ film = Blueprint("film", __name__)
 def list_films() -> Response:
     page = request.args.get("page", 1, type=int)
     per_page = request.args.get("per_page", 25, type=int)
-    films, total_count = get_paginated_films(page, per_page)
+    genre = request.args.get("genre", "", type=str)
+    films, total_count = get_paginated_films(page, per_page, genre)
     film_schemas = [FilmSchema.model_validate(film).model_dump() for film in films]
     return jsonify(
         items=film_schemas, pagination=create_pagination(page, per_page, total_count)
